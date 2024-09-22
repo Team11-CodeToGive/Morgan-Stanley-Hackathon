58d3151906b069593da3c0f2d0ac8ffda86db9d1
Jonathan <jongroberg@gmail.com>
2024-09-21 23:14:55 -0400

    tweaks styles of header footer and also for card, tweaked timeline to use new json format

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,29 +1,33 @@
+import { Bookmark, Share, } from "lucide-react"
+
 export default function EventCard({ event }) {
     return (
-        <div className="bg-white border-gray-200 border shadow-sm p-5 ">
+        <div className="py-5 cursor-pointer hover:bg-gray-50 bg-gray-50 hover:outline-gray-200 rounded-lg hover:shadow outline outline-gray-100 px-5"
+        onClick={()=>document.location.href= '/event-details'}>
             <div className="flex gap-4 ">
                 <span className="flex-col">
                     {/* <p>{event.thumbnail}</p> */}
-                    <p className="h-24 w-32  bg-gray-200"></p>
+                    <div className="h-20 w-36 rounded bg-gray-200"></div>
                 </span>
                 <span className="flex-col flex gap-1 py-1">
-                    <p className="text-sm">{event.startDateTime} - {event.endDateTime} </p>
-                    <h1>{event.title}</h1>
-                    <p className="text-sm">{event.location}</p>
+                    {/* <div className="text-sm">{event.startDateTime} - {event.endDateTime} </div> */}
+                    <div className="text-sm">{event.startDateTime} EST </div>
+                    <div className=" text-base font-semibold">{event.title}</div>
+                    <div className="text-sm">{event.location}</div>
                 </span>
 
             </div>
-            <span className="flex gap-4 ">
+            <span className="flex gap-4 mt-4 ">
                 <span className="flex gap-4">
-                    <p className=" w-32 "></p>
+                    <p className=" w-36 "></p>
                 </span>
-                <span className="flex w-full gap-1">
-                    <span className="flex gap-1 me-auto">
-                        <p>{event.attendees?.length || 0} Attendees: </p>
-                        {event.attendees && event.attendees.map((attendee, key) => <div className=" rounded-full bg-gray-200 w-6 h-6 text-center align-middle" key={key}>{attendee[0]}</div>)}
+                <span className="flex w-full gap-3">
+                    <span className="flex me-auto text-sm text-gray-500 items-center">
+                        <p className="me-5">{event.attendees?.length || 0} Attendees: </p>
+                        {event.attendees && event.attendees.map((attendee, key) => <div className="flex -ml-3 shadow outline outline-2 items-center justify-center font-mono rounded-full bg-accent text-gray-100 w-7 h-7" key={key}><div>{attendee[0]}</div></div>)}
                     </span>
-                    <div className="bg-blue-200 px-1 rounded shadow">bookMark</div>
-                    <div className="bg-blue-200 px-1 rounded shadow">share</div>
+                    <button className=" bg-white p-2 outline outline-gray-100 rounded-full hover:shadow-lg transition duration-100">{<Share />}</button>
+                    <button className="  bg-white p-2 outline outline-gray-100 rounded-full hover:shadow-lg transition duration-200">{<Bookmark />}</button>
                 </span>
             </span>
         </div>
