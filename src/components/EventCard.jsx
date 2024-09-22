3162f7a2332d9e9faae2c57894aaa8c8857b7046
Jonathan <jongroberg@gmail.com>
2024-09-22 14:54:19 -0400

    updated timeline to get active data

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,6 +1,6 @@
 import { Bookmark, Share, } from "lucide-react"
 import { navigate } from 'astro:transitions/client';
-
+import { toUserFriendlyTimeFormat } from '../utils/dateUtils';
 
 export default function EventCard({ event }) {
     return (
@@ -13,9 +13,9 @@ export default function EventCard({ event }) {
                 </span>
                 <span className="flex-col flex gap-1 py-1">
                     {/* <div className="text-sm">{event.startDateTime} - {event.endDateTime} </div> */}
-                    <div className="text-sm">{event.startDateTime} EST </div>
+                    <div className="text-sm">{toUserFriendlyTimeFormat(event.start_datetime)} EST </div>
                     <div className=" text-base font-semibold">{event.title}</div>
-                    <div className="text-sm">{event.location}</div>
+                    <div className="text-sm">{event.location.name}, {event.location.address}</div>
                 </span>
 
             </div>
@@ -25,7 +25,7 @@ export default function EventCard({ event }) {
                 </span>
                 <span className="flex w-full gap-3">
                     <span className="flex me-auto text-sm text-gray-500 items-center">
-                        <p className="me-5">{event.attendees?.length || 0} Attendees: </p>
+                        <p className="me-5">{event.attendees?.length || 0}  / {event.capacity} Attendees: </p>
                         {event.attendees && event.attendees.map((attendee, key) => <div className="flex -ml-3 shadow outline outline-2 items-center justify-center font-mono rounded-full bg-accent text-gray-100 w-7 h-7" key={key}><div>{attendee[0]}</div></div>)}
                     </span>
                     <button className=" bg-white p-2 outline outline-gray-100 rounded-full hover:shadow-lg transition duration-100">{<Share />}</button>
